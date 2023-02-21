35ce495c29770c2d95c25a805b50008f3b922349
Mehmet Umut Ayan <mehmetumutayan2007@gmail.com>
2023-02-21 21:19:59 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+
+   
+{
+    internal class MusteriManager
+    {
+
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Isim + "adlı müşteri başarıyla eklendi.");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Isim + "adlı müşteri başarıyla silindi.");
+        }
+
+        public void Kisisel(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Kişisel Bilgileri");       
+            Console.WriteLine("Ad: "+musteri.Isim);       
+            Console.WriteLine("Soyad: "+musteri.Soyisim);       
+            Console.WriteLine("Id: "+musteri.Id);
+            Console.WriteLine("Şehir: "+musteri.Sehir);
+        }
+
+    
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using ClassMetotDemo;
+
+Musteri musteri = new Musteri();
+
+
+Musteri musteri1= new Musteri();
+musteri1.Id = 198;
+musteri1.Isim = "Mehmet Umut";
+musteri1.Soyisim = "Ayan";
+musteri1.Sehir = "Istanbul";
+
+Musteri musteri2 = new Musteri();
+musteri2.Id = 19238;
+musteri2.Isim = "Ecrin";
+musteri2.Soyisim = "Ayan";
+musteri2.Sehir = "Istanbul";
+
+Musteri musteri3 = new Musteri();
+musteri3.Id = 1854328;
+musteri3.Isim = "Hamza";
+musteri3.Soyisim = "Ayan";
+musteri3.Sehir = "Istanbul";
+
+
+MusteriManager musteriManager = new MusteriManager();
+
+//musteriManager.Ekle(musteri1);
+musteriManager.Kisisel(musteri2);
+
+
+
+
